6c0d335778390f4cebc3f288b2b5bb9d5b0b8e4b
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-03-17 18:38:31 +0300

    Updated index.js && images

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ramens = [
-  { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-  { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-  { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+  { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "~/images/shoyu.jpg", rating: 5, comment: "Delicious!" },
+  { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "~/images/miso.jpg", rating: 4, comment: "Very flavorful!" },
+  { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "~/images/tonkotsu.jpg" }
 ];
 
 function displayRamens() {
